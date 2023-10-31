857b3e14618e49c7ded1d86feab030aea8359e41
estarhp <3136551640@qq.com>
2023-10-31 13:25:20 +0800

    feat: 精简昨日代码

--- 2023/10/30/compactObject.js
+++ 2023/10/30/compactObject.js
@@ -3,17 +3,6 @@
  * @return {Object|Array}
  */
 const compactObject = function(obj) {
- if (!Array.isArray(obj)){
-       Object.keys(obj).filter((item)=> {
-        if (obj[item] && typeof obj[item] === "object"){
-          obj[item] = compactObject(obj[item])
-         }else {
-         return !obj[item]
-        }
-      }).forEach((itme) => {
-         delete obj[itme]
-      })
-     }
  if (Array.isArray(obj)){
   const newobj = []
   obj.forEach((item,index)=> {
@@ -23,12 +12,20 @@ const compactObject = function(obj) {
     }else {
      newobj.push(item)
     }
-   
    }
-   })
-  obj = newobj
+  })
+  return newobj
  }
+
+ Object.keys(obj).filter((item)=> {
+  if (obj[item] && typeof obj[item] === "object"){
+   obj[item] = compactObject(obj[item])
+  }else {
+   return !obj[item]
+  }
+ }).forEach((itme) => {
+  delete obj[itme]
+ })
  return obj
 };
 
-console.log(compactObject([null, 0, 5, [0], [false, 16]]))
