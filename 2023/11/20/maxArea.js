43ba467d02dc24ca585c98ae159ccb03b39def31
estarhp <3136551640@qq.com>
2023-11-20 16:47:10 +0800

    feat: 2023/11/20

---
+++ 2023/11/20/maxArea.js
@@ -0,0 +1,73 @@
+// /**
+//  * @param {number[]} height
+//  * @return {number}
+//  */
+// const maxArea = function(height) {
+//
+//     let maxSize = 0
+//     let j = 0
+//     for (let i = 0; i < height.length - 1; i++) {
+//         let max = 0
+//         let min = 0
+//         let currentMin  = 0
+//         let currentMax = 0
+//         const curHeight = height[i]
+//         for (j = i + 1; j < height.length; j++){
+//             const nextHeight = height[j]
+//             if ( nextHeight > curHeight){
+//                 max = j
+//             }else {
+//                 const minSize = nextHeight * (j - i)
+//                 if ( minSize > currentMin){
+//                     currentMin = minSize
+//                     min = j
+//                 }
+//             }
+//         }
+//
+//
+//         if (max > min){
+//             currentMax = (max - i) * curHeight
+//         }else {
+//             currentMax = Math.max((max - i) * curHeight,currentMin)
+//         }
+//
+//         if (currentMax > maxSize) {
+//             maxSize = currentMax
+//         }
+//     }
+//
+//     return maxSize
+// };
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const maxArea = function(height) {
+    let maxSize = 0
+
+    let left = 0;
+    let right = height.length;
+
+    while(left < right) {
+        let currentMax = 0
+        const leftHeight = height[left]
+        const rightHeight = height[right]
+        if (leftHeight < rightHeight) {
+            currentMax = (right - left) * leftHeight
+            left ++
+        }else {
+            currentMax = (right - left) * rightHeight
+            right --
+        }
+
+        if (currentMax > maxSize) maxSize = currentMax
+    }
+
+    return maxSize
+
+};
+
+console.log(maxArea([1,2]))
+
--- 2023/11/20/moveZeroes.js
+++ 2023/11/20/moveZeroes.js
@@ -18,6 +18,33 @@ const moveZeroes = function(nums) {
     }
 
 };
+// /**
+//  * @param {number[]} nums
+//  * @return {void} Do not return anything, modify nums in-place instead.
+//  */
+// var moveZeroes = function(nums) {
+//     //左右指针 一个迭代 一个表示end
+//     //swap
+//     let left=0;
+//     let right=0;
+//
+//     function swap(left,right){
+//         nums[left]=nums[right];
+//     }
+//     while(right<nums.length){
+//         if(nums[right]!==0){
+//             swap(left,right)
+//             left++;
+//         }
+//         right++
+//     }
+//     while(left<nums.length){
+//         nums[left]=0
+//         left++;
+//     }
+//
+// };
+
 
 const nums = [1,0,0,2,3]
 moveZeroes(nums)
